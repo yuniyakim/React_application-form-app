f099db91e7e53017214370391c50e38e6728372e
Yuniya Kim <kim.yuniya@mail.ru>
2022-07-07 15:03:17 +0300

    Create application and form components

---
+++ src/components/Application.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ApplicationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Application = () => {
+  return (
+    <ApplicationContainer>
+      application
+    </ApplicationContainer>
+  );
+}
+
+export default Application;
---
+++ src/components/Form.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Form = () => {
+  return (
+    <FormContainer>
+      form
+    </FormContainer>
+  );
+}
+
+export default Form;

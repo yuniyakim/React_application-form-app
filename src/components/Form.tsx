b42b71ff6598f5647ad6617632a2c7eb2fd99667
Yuniya Kim <kim.yuniya@mail.ru>
2022-07-09 17:42:53 +0300

    Fix loading animation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import Button from "./Button";
 import Select from "./Select";
-import {InjectedFormProps, reduxForm, submit} from 'redux-form';
+import {InjectedFormProps, reduxForm, reset, submit} from 'redux-form';
 import {useDispatch} from 'react-redux';
 import TextField from "./TextField";
 import validate from "./validate";
@@ -24,16 +24,21 @@ const RowContainer = styled.div`
 
 const FormComponent: React.FC<InjectedFormProps> = (props) => {
   const dispatch = useDispatch();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const onSubmitClick = () => {
-    dispatch(submit("ApplicationForm"));
+    setIsSubmitting(true);
+    setTimeout(() => {
+      dispatch(submit("ApplicationForm"));
+      setIsSubmitting(false);
+    },2000);
   };
 
   return (
     <FormContainer>
       <RowContainer>
         <TextField fieldName="name" label="Ваше имя *" placeholder="Иван" size="small" />
-        <TextField fieldName="phone" label="Номер телефона *" placeholder="+7 (000) 000-00-00" size="small" />
+        <TextField fieldName="phone" label="Номер телефона *" placeholder="+7 (000) 000-00-00" size="small" mask="+7 (999) 999-99-99" />
       </RowContainer>
       <RowContainer>
         <TextField fieldName="email" label="E-mail *" placeholder="example@skdesign.ru" size="small" />
@@ -43,24 +48,26 @@ const FormComponent: React.FC<InjectedFormProps> = (props) => {
       <TextField fieldName="company" label="Название организации/студии" placeholder="SK Design" size="medium" />
 
 
+
       <TextField fieldName="recipient" label="Получатель" placeholder="ФИО" size="medium" />
       <Select fieldName="resource" label="Откуда узнали про нас?" selectValues={['one', 'two', 'three']} size="medium" />
-      <Button disabled={props.pristine || props.invalid} onClick={onSubmitClick} loading size="medium">
+      <Button disabled={props.pristine || props.invalid} onClick={onSubmitClick} loading={isSubmitting} size="medium">
         Отправить заявку
       </Button>
     </FormContainer>
   );
 }
 
-const showResults = (values: Object) => {
-  setTimeout(() => {
-      console.log(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
-  }
-  ,1000);
+const logResults = (values: Object) => {
+  console.log(JSON.stringify(values, null, 2));
 };
+
+const resetForm = (result: Object, dispatch: Function) =>
+  dispatch(reset('ApplicationForm'));
 
 export const Form = reduxForm<{}>({
   form: "ApplicationForm",
-  onSubmit: showResults,
+  onSubmit: logResults,
+  onSubmitSuccess: resetForm,
   validate
 })(FormComponent);
